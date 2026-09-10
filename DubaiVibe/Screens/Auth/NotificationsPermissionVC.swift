1b8e42323b0870e4b17469da105d6495481c15a7
TusharAnawadiya <tusharanawadiya444@gmail.com>
2026-09-10 15:30:40 +0530

    - change notification permission image - change notification permission button hide

--- DubaiVibe/Screens/Auth/NotificationsPermissionVC.swift
+++ DubaiVibe/Screens/Auth/NotificationsPermissionVC.swift
@@ -4,7 +4,6 @@ final class NotificationsPermissionVC: UIViewController {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var subtitleLabel: UILabel!
     @IBOutlet private weak var enableButton: GoldGradientButton!
-    @IBOutlet private weak var laterButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,19 +13,12 @@ final class NotificationsPermissionVC: UIViewController {
         enableButton?.titleLabel?.font = .systemFont(ofSize: 17, weight: .semibold)
         enableButton?.clipsToBounds = true
         enableButton?.layer.cornerRadius = 14
-
-        laterButton?.clipsToBounds = true
-        laterButton?.layer.cornerRadius = 14
     }
 
     @IBAction private func enableTapped(_ sender: Any) {
         FCMNotificationManager.requestAuthorizationIfNeeded { [weak self] _ in
             self?.finishOnboarding()
         }
-    }
-
-    @IBAction private func laterTapped(_ sender: Any) {
-        finishOnboarding()
     }
 
     private func finishOnboarding() {
